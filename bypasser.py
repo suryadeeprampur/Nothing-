d33f3dee48b7bd5c36b3ce1c755f08691e9abda1
suryadeeprampur <suryadeep.rampur@gmail.com>
2026-02-21 13:21:50 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2138,7 +2138,7 @@ def lksfycom(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://main.sportswordz.com"
+    ref = "https://sportswordz.com"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
@@ -3182,4 +3182,5 @@ def shortners(url):
 
 
 ##########################################################################
+
 
